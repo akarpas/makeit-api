28461eda8fce7605d0dbdc7bcdef74795b1f8737
Andreas Karpasitis <andreasmkar@gmail.com>
2017-11-12 14:30:42 +0100

    add basic routes to user

--- server/api/user/userModel.js
+++ server/api/user/userModel.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const userSchema = new Schema({
+  userid: String,
   name: String,
   lastname: String,
   email: String,
--- server/api/user/userRoutes.js
+++ server/api/user/userRoutes.js
@@ -1,14 +1,32 @@
 const router = require('express').Router()
 const bodyParser = require('body-parser')
 const app = express()
+const User = require('./userModel.js')
 const _ = require('lodash')
 
 app.use(express.static('client'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-router.get('/users', (req, res, next) => {
-  res.json()
+// this may need to be '/users'
+router.get('/', (req, res, next) => {
+  if (err) next(err)
+  User.find({}, (err, users) => {
+    res.json(users)
+  })
 })
+
+router.get('/:id', (req, res, next) => {
+  if (err) next(err)
+  User.find({ userid: req.params.id }, (err, user) => {
+    res.json(user)
+  })
+})
+
+router.post('/', (req, res, next) => {})
+
+router.put('/:id', (req, res, next) => {})
+
+router.delete('/:id', (req, res, next) => {})
 
 module.exports = router
